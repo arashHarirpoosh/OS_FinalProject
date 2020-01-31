0a451fb44c9495f55d7ed607beb0b053c5510771
Matin Tavakoli <matin1378@aut.ac.ir>
2020-02-01 00:51:28 +0330

    readerWriter.c comments added

--- xv6/xv6-public/readerWriter.c
+++ xv6/xv6-public/readerWriter.c
@@ -13,7 +13,7 @@ int main () {
 
     printf(1, "enter pattern for readers/writers test\n");
     int read_size;
-    read_size = read(0, argv, sizeof(argv));
+    read_size = read(0, argv, sizeof(argv));//reading pattern
     argv[read_size - 1] = '\0';
     int pattern[100], i;
     for (i = 0; argv[i + 1] != '\0'; i++) {
@@ -32,7 +32,7 @@ int main () {
 }
 void testReaderWriters(int* pattern, int pattern_size){
     int pid, i, res;
-    rwinit();
+    rwinit();//initializes reader writer
     pid = fork();
     for (i = 1; i < pattern_size; i++){
         if (pid <0){
@@ -50,19 +50,19 @@ void testReaderWriters(int* pattern, int pattern_size){
     } else if (pid == 0) {
         printf(1, "child adding to shared counter %d\n", pattern[i-1]);
         res = rwtest(pattern[i-1]);
-        if (pattern[i-1] == 0){
+        if (pattern[i-1] == 0){//reader part
             printf(1, "reader read from shared counter\n");
-        } else{
+        } else{//writer part
             printf(2, "writer added to shared counter\n");
         }
         exit();
     } else{
-        for (int i = 0; i < pattern_size; i++) {
+        for (int i = 0; i < pattern_size; i++) {//wait for the forked processes to exit
             wait();
         }
         printf(1, "user program finished\n");
         res = rwtest(0);
-        printf(1, "last value of shared counter : %d\n", res);
+        printf(1, "last value of shared counter : %d\n", res);//prints last value of shared counter
     }
     exit();
 }
