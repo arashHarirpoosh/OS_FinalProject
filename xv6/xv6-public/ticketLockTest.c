643a0a3977741e7d7d3d43bcc482e88c68cb0f67
Matin Tavakoli <matin1378@aut.ac.ir>
2020-02-01 00:46:06 +0330

    ticketLockTest.c comments added

--- xv6/xv6-public/ticketLockTest.c
+++ xv6/xv6-public/ticketLockTest.c
@@ -1,6 +1,6 @@
 //
 // Created by arash on 2020-01-24.
-//
+//testing ticket lock!
 
 #include "types.h"
 #include "user.h"
@@ -11,7 +11,7 @@
 int main ()
 {
     int pid;
-    ticketlockInit();
+    ticketlockInit();//initializes ticket lock
     pid = fork();
     for (int i = 1; i < NCHILD; i++) {
         if (pid < 0){
@@ -26,14 +26,14 @@ int main ()
         exit();
     } else if (pid == 0){
         printf(1, "child adding t shared counter\n");
-        printf(1,"%d\n",ticketlockTest());
+        printf(1,"%d\n",ticketlockTest());//prints value of ticket lock
         exit();
     } else{
-        for (int i = 0; i < NCHILD; i++) {
+        for (int i = 0; i < NCHILD; i++) {//waits for the forked processes to exit
             wait();
         }
         printf(1, "user program finished\n");
-        printf(2, "ticket lock value: %d\n", ticketlockTest() - 1);
+        printf(2, "ticket lock value: %d\n", ticketlockTest() - 1);//prints the most recent ticket lock value
     }
     exit();
 
